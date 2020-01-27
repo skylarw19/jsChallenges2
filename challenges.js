67c95b7c04234d07e4f4e0f965ec9d6f8b665d01
Skylar Wong <skylar@Skylars-MacBook-Air.local>
2020-01-27 16:14:20 -0600

    solve challenge 02

--- challenges.js
+++ challenges.js
@@ -59,7 +59,12 @@ addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
 
+function addTwoNumbers(n1,n2){
+  if (typeof n1 === 'number' && typeof n2 === 'number')
+    return n1+n2;
+  else return NaN;
 
+}
 
 
 
